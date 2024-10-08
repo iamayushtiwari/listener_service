3630c63e62074da91e7f27fea01e935bcef93f94
Ayush Tiwari <ayushtiwari653@gmail.com>
2024-10-09 03:22:02 +0530

    docker setup

--- config/redis.js
+++ config/redis.js
@@ -1,12 +1,12 @@
 const Redis = require('redis')
-const redisClient = Redis.createClient()
+const redisClient = Redis.createClient({ url: 'redis://host.docker.internal:6379', })
 
 redisConnect().catch(err => console.log(err));
 
 async function redisConnect() {
     await redisClient.on('error', err => console.log('Redis Client Error', err))
         .connect();
-        console.log('Redis Client connected')
+    console.log('Redis Client connected')
 }
 
 module.exports = { redisClient }
